ae3a4b0be18c5f6c57f7a6617b42d7a6540ac8e3
Vishal Pandit <Vishalkumar14471@gmail.com>
2025-01-22 20:33:06 +0530

    Changed to signinwithPopup

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,17 +3,28 @@ import logo from '../Components/Images/logo-icon.png'
 import goole from '../Components/Images/google-icon.png'
 import { auth } from "../../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { GoogleAuthProvider, signInWithRedirect } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
 const Navbar = () => {
 
   const [user] = useAuthState(auth);
-  const googleSignIn = () => {
+  const googleSignIn = async () => {
     const provider = new GoogleAuthProvider();
-    signInWithRedirect(auth, provider);
+    try {
+      await signInWithPopup(auth, provider);
+      console.log("Google Sign-In Success");
+    } catch (error) {
+      console.error("Error signing in with Google: ", error);
+    }
   };
-  const signOut = () => {
-    auth.signOut();
+  
+  const signOutUser = async () => {
+    try {
+      await signOut(auth);
+      console.log("Sign-Out Success");
+    } catch (error) {
+      console.error("Error signing out: ", error);
+    }
   };
   
   return (
@@ -27,7 +38,7 @@ const Navbar = () => {
         {
           user?
         
-        <button onClick={signOut} className='flex justify-between my-6 ml-0 mr-[5%]  p-3 text-white border border-white hover:bg-gray-400'>
+        <button onClick={signOutUser} className='flex justify-between my-6 ml-0 mr-[5%]  p-3 text-white border border-white hover:bg-gray-400'>
         <div className='mt-1 font-2xl font-serif '>Sign Out</div>
         </button>
         :  
